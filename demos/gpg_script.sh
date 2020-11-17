f72bf73028869d2621efb57eadffd469d4450534
Alvaro Lupercio <lcodesignx@gmail.com>
2020-11-17 11:28:57 -0500

    adds script to set system ip address statically

--- demos/gpg_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Program used to export gpg keys
-ID='lupera1'
-
-gpg --export-secret-keys $ID > my-private-key.asc
--- loops/demo2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# loops demo
-for NUM in 1 2 3
-do
-    echo "file$NUM"
-done
